7feaac5480900c6bb595bae0394429a00a5a7c08
DmytroLomako <lomakodmytro@gmail.com>
2024-06-20 13:53:09 +0300

    add move

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from turtle import *
 speed(100)
 pensize(5)
-list = [0,0,1,
+list = [0,0,0,
         0,0,0,
-        2,0,0]
+        0,0,0]
+turn = 1
 def cross(x, y):
     # to center
     penup()
@@ -31,8 +32,8 @@ def cross(x, y):
 
 def zero():
     color('green')
-    forward(25)
     penup()
+    forward(25)
     left(90)
     forward(8)
     right(90)
@@ -41,12 +42,12 @@ def zero():
     penup()
     right(90)
     forward(8)
-    pendown()
     left(90) 
     left(180)
     forward(25)
     left(180)
     color('black')
+    pendown()
 
 def draw_figure(size, count):
     for i in range(count):
@@ -62,15 +63,91 @@ def draw_field(x, y):
         pendown()
         for j in range(3):
             draw_figure(50, 4)
-            index = j + i*3
-            if list[index] == 1:
-                x_angle = x + 50*j
-                y_angle = y - 50*i
-                cross(x_angle + 25, y_angle + 25)
-            elif list[index] == 2:
-                zero()
             forward(50)
 
-draw_field(-50, 50)
+draw_field(0, 0)
+text = Turtle()
+text.penup()
+text.goto(-20, 100)
+text.color('red')
+text.write('Черга ходу крестика', font = ("Arial", 16, "bold"))
 
-exitonclick()
+draw = False
+def check_win():
+    global draw
+    win = None
+    if list[0] != 0 and list[0] == list[1] and list[0] == list[2]:
+        win = list[0]
+    elif list[3] != 0 and list[3] == list[4] and list[3] == list[5]:
+        win = list[3]
+    elif list[6] != 0 and list[6] == list[7] and list[6] == list[8]:
+        win = list[6]
+    elif list[0] != 0 and list[0] == list[3] and list[0] == list[6]:
+        win = list[0]
+    elif list[1] != 0 and list[1] == list[4] and list[1] == list[7]:
+        win = list[1]
+    elif list[2] != 0 and list[2] == list[5] and list[2] == list[8]:
+        win = list[2]
+    elif list[0] != 0 and list[0] == list[4] and list[0] == list[8]:
+        win = list[0]
+    elif list[2] != 0 and list[2] == list[4] and list[2] == list[6]:
+        win = list[2]
+    print(list)
+    if win == 1:
+        text.clear()
+        text.color('red')
+        text.write('Переміг крестик', font = ("Arial", 20, "bold"))
+        draw = True
+    elif win == 2:
+        text.clear()
+        text.color('green')
+        text.write('Переміг нолик', font = ("Arial", 20, "bold"))
+        draw = True
+
+def click(x, y):
+    global turn, draw
+    if draw == False:
+        if x >= 0 and x <= 50:
+            x = 0
+        elif x > 50 and x <= 100:
+            x = 1
+        elif x > 100 and x <= 150:
+            x = 2
+        else:
+            x = None
+        if y <= 50 and y >= 0:
+            y = 0
+        elif y < 0 and y >= -50:
+            y = 1
+        elif y <-50 and y >= -100:
+            y = 2
+        else:
+            y = None
+        if x != None and y != None:
+            index = x + y*3
+            if list[index] == 0:
+                if turn == 1:
+                    draw = True
+                    list[index] = 1
+                    turn = 0
+                    text.clear()
+                    text.color('green')
+                    text.write('Черга ходу нолика', font = ("Arial", 16, "bold"))
+                    cross(x * 50 + 25, y * -50 + 25)
+                    draw = False
+                elif turn == 0:
+                    draw = True
+                    list[index] = 2
+                    penup()
+                    goto(x * 50, y * -50)
+                    text.clear()
+                    text.color('red')
+                    text.write('Черга ходу крестика', font = ("Arial", 16, "bold"))
+                    turn = 1
+                    zero()
+                    draw = False
+                check_win()
+        print(x, y)
+onscreenclick(click)
+
+mainloop()
